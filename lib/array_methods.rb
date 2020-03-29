ca7c4525061705eb3f63f1090436c0f593603a2d
Shiyun Lu <lushiyun@outlook.com>
2020-03-29 21:01:25 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,21 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
+  # array.index(value_to_find)
+  
+  array.length.times do |index|
+    if value_to_find == array[index]
+      return index
+    end
+  end
+  
+  nil
+  
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
