0974707927bb7d65e4c5874ee495ace740106133
Shiyun Lu <lushiyun@outlook.com>
2020-03-29 23:05:48 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,9 +13,33 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.max
+  # array.max
+  
+  if array.length < 1
+    nil
+  end
+  
+  max = array[0]
+  array.length.times do |cur_index|
+    if (array[cur_index] > max)
+      max = array[cur_index]
+    end
+  end
+  max
 end
 
 def find_min_value(array)
-  array.min
+  # array.min
+  
+  if array.length < 1
+    nil
+  end
+  
+  min = array[0]
+  array.length.times do |cur_index|
+    if (array[cur_index] < min)
+      min = array[cur_index]
+    end
+  end
+  min
 end
